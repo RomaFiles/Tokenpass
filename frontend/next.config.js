4c78102d2080034b5c283e08b606874b58f08863
RomaFiles <roma.mb89@gmail.com>
2025-11-09 20:48:33 -0600

    feat: update next.config.js to enable Turbopack and set page directory

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  turbopack: {}, // ✅ activa Turbopack
+  experimental: { turbo: false },
+  // 👇 Le dices a Next que la raíz de las páginas está en /src
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  dir: './src',
 };
 
 module.exports = nextConfig;
